0aab825a3fa554525312571307e8fa9dd5512df2
Oleg_Levchenko <Leuchankaaleh@gmail.com>
2025-03-29 01:25:43 +0100

    �������������������� ������������ ���������������� ������������������: ���������������� �������������������� XAML, ���������������� BooleanInvertConverter

---
+++ src/Client/HairCarePlus.Client.Patient/Features/Calendar/Helpers/BooleanInvertConverter.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace HairCarePlus.Client.Patient.Features.Calendar.Helpers
+{
+    public class BooleanInvertConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return !boolValue;
+            }
+            
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return !boolValue;
+            }
+            
+            return false;
+        }
+    }
+} 
